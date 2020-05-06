9722742166bc8e889463816454be453a5a7f7d29
sangTran <sangthien2010@gmail.com>
2020-05-06 18:32:31 -0500

    deploy the prod version of app and add header access control

--- handler.js
+++ handler.js
@@ -1,12 +1,19 @@
 "use strict";
 
+const emojis = ["😀", "😄", "😏", "😎", "😍", "🥉", "🥈", "🥇", "🏆"];
+
 module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify(
       {
         message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+        input: rankEmoji,
       },
       null,
       2
